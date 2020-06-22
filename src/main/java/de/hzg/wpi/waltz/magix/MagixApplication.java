39c90ed6ac21036ca8360a33876123d0e2a8808f
ingvord <tla-ingvord@yandex.ru>
2020-06-22 14:07:57 +0300

    progress

--- src/main/java/de/hzg/wpi/waltz/magix/MagixApplication.java
+++ src/main/java/de/hzg/wpi/waltz/magix/MagixApplication.java
@@ -1,5 +1,7 @@
 package de.hzg.wpi.waltz.magix;
 
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
@@ -16,8 +18,20 @@ public class MagixApplication extends Application {
     public Set<Object> getSingletons() {
         Set<Object> singletons = new HashSet<>();
 
+        singletons.add(getCorsFilter());
+
         singletons.add(new MagixRestService());
 
         return singletons;
+    }
+
+    private CorsFilter getCorsFilter() {
+        CorsFilter cors = new CorsFilter();
+        cors.getAllowedOrigins().add("*");
+        cors.setAllowCredentials(true);
+        cors.setAllowedMethods("GET,POST,PUT,DELETE,HEAD");
+        cors.setCorsMaxAge(1209600);
+        cors.setAllowedHeaders("Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization,Accept-Encoding,Accept-Language,Access-Control-Request-Method,Cache-Control,Connection,Host,Referer,User-Agent");
+        return cors;
     }
 }
--- src/main/java/de/hzg/wpi/waltz/magix/MagixRestService.java
+++ src/main/java/de/hzg/wpi/waltz/magix/MagixRestService.java
@@ -1,5 +1,8 @@
 package de.hzg.wpi.waltz.magix;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,6 +19,8 @@ import javax.ws.rs.sse.SseEventSink;
  */
 @Path("/")
 public class MagixRestService {
+    private final Logger logger = LoggerFactory.getLogger(MagixRestService.class);
+
     private volatile SseBroadcaster broadcaster;
 
     @Context
@@ -26,6 +31,7 @@ public class MagixRestService {
     @POST
     @Path("/broadcast")
     public void post(String message, @Context Sse sse) {
+        logger.debug("broadcasting message {}", message);
         broadcaster.broadcast(sse.newEvent(message));
     }
 
@@ -33,6 +39,7 @@ public class MagixRestService {
     @Path("/subscribe")
     @Produces(MediaType.SERVER_SENT_EVENTS)
     public void getStream(@Context SseEventSink sink) {
+        logger.debug("Subscribing new client...");
         broadcaster.register(sink);
     }
 }
