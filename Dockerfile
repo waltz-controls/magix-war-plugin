511a19f220afabda5f47e0d2075f5e0707d1cc57
ingvord <igor.khokhriakov@rcsb.org>
2022-07-05 13:51:45 -0700

    Switch to apm agent

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM tomcat:9-jre11-openjdk-slim
 
+COPY elastic-apm-agent-1.32.0.jar /usr/local/tomcat
+
+COPY setenv.sh /usr/local/tomcat/bin
+
 COPY target/magix.war /usr/local/tomcat/webapps
---
+++ setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JAVA_OPTS="$JAVA_OPTS \
+                  -javaagent:/usr/local/tomcat/elastic-apm-agent-1.32.0.jar \
+                  -Delastic.apm.service_name=magix \
+                  -Delastic.apm.server_urls=http://apm-server:8200 \
+                  -Delastic.apm.secret_token= \
+                  -Delastic.apm.environment=production \
+                  -Delastic.apm.application_packages=de.hzg.wpi.waltz"
