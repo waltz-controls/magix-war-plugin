d5c9d3df1631dc1c2d4e8a698fa1736de42c4ce7
ingvord <igor.khokhriakov@rcsb.org>
2022-07-05 14:05:44 -0700

    Switch to apm agent

--- setenv.sh
+++ setenv.sh
@@ -3,7 +3,7 @@
 export JAVA_OPTS="$JAVA_OPTS \
                   -javaagent:/usr/local/tomcat/elastic-apm-agent-1.32.0.jar \
                   -Delastic.apm.service_name=magix \
-                  -Delastic.apm.server_urls=http://apm-server:8200 \
+                  -Delastic.apm.server_urls=http://helm-apm-server-default-apm-server.kube-system.svc.cluster.local:8200 \
                   -Delastic.apm.secret_token= \
                   -Delastic.apm.environment=production \
                   -Delastic.apm.application_packages=de.hzg.wpi.waltz"
